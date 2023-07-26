402bf61dcdf0ef5480b9589a0e95ee4300703c0e
Keita Watanabe <mlkeita@amazon.com>
2023-07-26 02:59:09 +0000

    update with profiler

--- 3.test_cases/3.MPT/slurm-scripts/run-composer.sh
+++ 3.test_cases/3.MPT/slurm-scripts/run-composer.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-echo "Hello, I am $(hostname)"
-echo "Run composer with args: "
 set -euxo pipefail
 
 export WORLD_SIZE=$1
@@ -9,6 +7,7 @@ export NODE_RANK=$3
 export MASTER_ADDR=$4
 export MASTER_PORT=$5
 
+echo "Hello, I am $(hostname), processing rank ${NODE_RANK}."
 echo "Set Environment variables for distributed training"
 export FI_EFA_USE_DEVICE_RDMA=1 # use for p4d
 export FI_EFA_FORK_SAFE=1
@@ -26,20 +25,31 @@ export NCCL_DEBUG=INFO
 
 echo "Sanity check"
 nvidia-smi
+PROFILE_FILE=/fsx/logs/profile
 
-composer \
+# if [ ${NODE_RANK} -eq 0 ]; then
+#     # Use nvidia-nsight only on RANK 0
+#     mkdir -p /fsx/logs
+#     PROFILER="nsys profile -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu --capture-range=cudaProfilerApi --cudabacktrace=true -x true -o /fsx/logs/profile-rank-${NODE_RANK}"
+# else
+#     PROFILER=""
+# fi
+mkdir -p /fsx/logs
+MODEL=mpt-7b
+PROFILER="nsys profile --force-overwrite=true  -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu --capture-range=cudaProfilerApi --cudabacktrace=true -x true -o /fsx/logs/profile-mpt30b-rank-${NODE_RANK}"
+start=$(date +%s)
+nsys profile  --duration 300 --delay 30 --force-overwrite=true -w true -t cuda,nvtx,osrt,cudnn,cublas -s cpu -x true -o /fsx/logs/${MODEL}-p4d-worldsize-${WORLD_SIZE}-rank-${NODE_RANK}.profile composer \
     --world_size ${WORLD_SIZE} \
     --nproc ${N_PROC} \
     --node_rank ${NODE_RANK} \
     --master_addr ${MASTER_ADDR} \
     --master_port ${MASTER_PORT} \
-    --verbose /llm-foundry/scripts/train/train.py \
-    /llm-foundry/scripts/train/yamls/pretrain/mpt-7b.yaml \
+    --verbose /apps/reference-architectures/3.test_cases/3.MPT/llm-foundry/scripts/train/train.py \
+    /llm-foundry/scripts/train/yamls/pretrain/${MODEL}.yaml \
     data_local=/fsx/my-copy-c4 \
     train_loader.dataset.split=train_small \
     eval_loader.dataset.split=val_small \
     max_duration=3ba \
     eval_interval=0 \
-    save_folder=mpt-7b \
-    device_train_microbatch_size=8 \
-    global_train_batch_size=256 
+    save_folder=${MODEL}
+echo "Elapsed Time: $(($end-$start)) seconds"
