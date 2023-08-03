609311d435ded425b14a4fec37dc7b4a28154c6c
Ankur Srivastava <awsankur@amazon.com>
2023-08-03 15:01:54 -0700

    Updated

--- 3.test_cases/4.DDP/5-create-key.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-aws ec2 create-key-pair --key-name pcluster-key --query KeyMaterial --output text > pcluster-key.pem
-sudo chmod 600 pcluster-key.pem
--- 3.test_cases/4.DDP/7-connect-to-cluster.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pcluster ssh -i 'pcluster-key.pem' -n pcluster-ml
