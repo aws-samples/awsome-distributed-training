9c60fe0fdac343323652b8bdd178b64c5e4fbe89
Pierre-Yves Aquilanti <pierreya@amazon.com>
2023-08-15 10:07:32 -0500

    Update documentation and remove additional scripts for parallelcluster archs

--- 1.architectures/2.aws-parallelcluster/create-cluster.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pcluster create-cluster --cluster-configuration pcluster-config.yaml --cluster-name pcluster-ml --region us-west-2 --suppress-validators "type:InstanceTypeBaseAMICompatibleValidator" --rollback-on-failure "false"
