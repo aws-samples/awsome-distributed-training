4eba76c0e61db8559b9468d80fe1e8076b11db49
Pierre-Yves Aquilanti <pierreya@amazon.com>
2023-08-15 10:38:38 -0500

    Remove config with grafana files for cleanup

--- 1.architectures/2.aws-parallelcluster/config_with_grafana_monitoring/create-additional-sg.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "Please enter the vpc id of your cluster: " vpc_id
-echo -e "creating a security group with $vpc_id..."
-security_group=$(aws ec2 create-security-group --group-name grafana-sg --description "Open HTTP/HTTPS ports" --vpc-id ${vpc_id} --output text)
-aws ec2 authorize-security-group-ingress --group-id ${security_group} --protocol tcp --port 443 --cidr 0.0.0.0/0
-aws ec2 authorize-security-group-ingress --group-id ${security_group} --protocol tcp --port 80 —-cidr 0.0.0.0/0
