8c49dec05c675ebbae7a12c000b4d34f13a28fd1
Verdi March <marcverd@amazon.com>
2023-08-30 12:58:06 +0000

    nemo-launcher: working, with dummy dataset

--- 3.test_cases/2.nemo-launcher/step-00-bootstrap-launcher.sh
+++ 3.test_cases/2.nemo-launcher/step-00-bootstrap-launcher.sh
@@ -7,13 +7,15 @@
 
 set -exuo pipefail
 
+NEMO_LAUNCHER_VERSION=23.07
 srun -N 1 \
-    --container-mounts=/fsx/ubuntu/sample-slurm-jobs/nemo-launcher-23.05:/workspace/mount_dir \
-    --container-image=/fsx/ubuntu/aws-nemo-megatron_23.05-py3.sqsh \
+    --container-mounts=/fsx/ubuntu/sample-slurm-jobs/nemo-launcher-$NEMO_LAUNCHER_VERSION:/workspace/mount_dir \
+    --container-image=/fsx/ubuntu/aws-nemo-megatron_$NEMO_LAUNCHER_VERSION-py3.sqsh \
     bash -c "cp -a /opt/NeMo-Megatron-Launcher/launcher_scripts /opt/NeMo-Megatron-Launcher/auto_configurator /opt/FasterTransformer /workspace/mount_dir/"
 
-cd /fsx/ubuntu/sample-slurm-jobs/nemo-launcher-23.05/
+cd /fsx/ubuntu/sample-slurm-jobs/nemo-launcher-$NEMO_LAUNCHER_VERSION/
 /usr/bin/python3 -m venv .venv
-source /fsx/ubuntu/sample-slurm-jobs/nemo-launcher-23.05/.venv/bin/activate
-curl -LO https://raw.githubusercontent.com/NVIDIA/NeMo-Megatron-Launcher/23.05/requirements.txt
+source /fsx/ubuntu/sample-slurm-jobs/nemo-launcher-$NEMO_LAUNCHER_VERSION/.venv/bin/activate
+curl -LO https://raw.githubusercontent.com/NVIDIA/NeMo-Megatron-Launcher/$NEMO_LAUNCHER_VERSION/requirements.txt
+pip3 install --upgrade pip setuptools
 pip3 install -r requirements.txt
