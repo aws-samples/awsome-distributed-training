1efdaf80438bfe20637331b4c9a47af5be5cba77
Verdi March <marcverd@amazon.com>
2023-09-05 17:54:35 +0800

    Update paths in nemo-launcher bootstrap scripts

--- 3.test_cases/2.nemo-launcher/0.bootstrap-launcher.sh
+++ 3.test_cases/2.nemo-launcher/0.bootstrap-launcher.sh
@@ -7,15 +7,20 @@
 
 set -exuo pipefail
 
-NEMO_LAUNCHER_VERSION=23.07
+: "${NEMO_VERSION:=23.07}"
+: "${REPO:=aws-nemo-megatron}"
+: "${TAG:=$NEMO_VERSION-py3}"
+: "${ENROOT_IMAGE:=/apps/${REPO}_${TAG}.sqsh}"
+: "${TARGET_PATH:=/fsx/nemo-launcher-$NEMO_VERSION}"   # must be a shared filesystem
+
 srun -N 1 \
-    --container-mounts=/fsx/ubuntu/sample-slurm-jobs/nemo-launcher-$NEMO_LAUNCHER_VERSION:/workspace/mount_dir \
-    --container-image=/fsx/ubuntu/aws-nemo-megatron_$NEMO_LAUNCHER_VERSION-py3.sqsh \
+    --container-mounts=$TARGET_PATH:/workspace/mount_dir \
+    --container-image=$ENROOT_IMAGE \
     bash -c "cp -a /opt/NeMo-Megatron-Launcher/launcher_scripts /opt/NeMo-Megatron-Launcher/auto_configurator /opt/FasterTransformer /workspace/mount_dir/"
 
-cd /fsx/ubuntu/sample-slurm-jobs/nemo-launcher-$NEMO_LAUNCHER_VERSION/
-/usr/bin/python3 -m venv .venv
-source /fsx/ubuntu/sample-slurm-jobs/nemo-launcher-$NEMO_LAUNCHER_VERSION/.venv/bin/activate
-curl -LO https://raw.githubusercontent.com/NVIDIA/NeMo-Megatron-Launcher/$NEMO_LAUNCHER_VERSION/requirements.txt
-pip3 install --upgrade pip setuptools
-pip3 install -r requirements.txt
+cd $TARGET_PATH
+/usr/bin/python3.8 -m venv .venv
+source $TARGET_PATH/.venv/bin/activate
+curl -LO https://raw.githubusercontent.com/NVIDIA/NeMo-Megatron-Launcher/$NEMO_VERSION/requirements.txt
+pip3.8 install --upgrade pip setuptools
+pip3.8 install -r requirements.txt
