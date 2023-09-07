fbd637450886b7d4f5304af863193eb737dafb47
Pierre-Yves Aquilanti <pierreya@amazon.com>
2023-09-07 17:06:26 +0000

    Delete deploy.sh for VPC

--- 1.architectures/1.vpc_network/deploy.sh
+++
@@ -1,6 +0,0 @@
-aws cloudformation create-stack --stack-name vpc-stack-ml\
-                                --template-body file://2.vpc-one-az.yaml \
-                                --parameters ParameterKey=SubnetsAZ,ParameterValue=us-west-2a \
-                                             ParameterKey=VPCName,ParameterValue="ML HPC VPC" \
-                                --capabilities CAPABILITY_IAM
-
