ef8086997d48e25834945f51d8c1cc0df0517a21
Alex Iankoulski <iankouls@amazon.com>
2023-09-11 22:54:26 -0700

    CDK app with configurable EKS cluster stack

--- 1.architectures/4.aws-eks/app.py
+++ 1.architectures/4.aws-eks/app.py
@@ -5,7 +5,6 @@ import aws_cdk as cdk
 
 from cluster.cluster_stack import ClusterStack
 
-
 app = cdk.App()
 ClusterStack(app, "ClusterStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
@@ -15,14 +14,15 @@ ClusterStack(app, "ClusterStack",
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
 
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
+    #env=cdk.Environment(account='************', region='us-west-2'),
 
     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+
     )
 
 app.synth()
