71b11d968419c7721c84d62db24de4928f1ac6cc
Verdi March <marcverd@amazon.com>
2023-09-13 08:38:11 +0000

    Example of gpt-5b; improve docs on nemo launcher mechanics

--- 3.test_cases/2.nemo-launcher/1.bmk-pretrain-gpt3.sh
+++ 3.test_cases/2.nemo-launcher/1.bmk-pretrain-gpt3.sh
@@ -3,20 +3,45 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set -exuo pipefail
+set -exo pipefail
+[[ -z "${TARGET_PATH}" ]] \
+    && { echo Please set environment variable TARGET_PATH ; exit 1 ; } \
+    || echo TARGET_PATH=$TARGET_PATH
+set -u
 
-: "${MODEL:=gpt3}"
-: "${MODEL_SIZE:=5b}"
-: "${NUM_NODES:=8}"
-: "${RUNTIME:=4h}"
-: "${MAX_STEPS:=5}"
-: "${TARGET_PATH:=/fsx/nemo-launcher-23.07}"
-WORKSPACE_CONT=$TARGET_PATH
-CONT_RESULT_DIR=${WORKSPACE_CONT}/results
-CONT_TOKENIZER_DIR=${WORKSPACE_CONT}/data/bpe
 
-: "${UNIQUE_OUTPUT_DIR:=0}"
+################################################################################
+# 000: Modify this section to define pre-training configuration: model size,
+# number of nodes, max. pre-training steps, job's max. runtime.
+################################################################################
+## Pre-train gpt3-126m on 2 nodes for 40 steps.
+export MODEL_SIZE=126m
+export NUM_NODES=2
+export RUNTIME=30m
+export MAX_STEPS=40
+declare -a MODEL_ARGS=()
 
+## Uncomment below to pre-train gpt3-5b on 2 nodes for 5 steps
+# export MODEL=gpt3
+# export MODEL_SIZE=5b
+# export NUM_NODES=2
+# export RUNTIME=4h
+# export MAX_STEPS=5
+## When node_count < 8, needs full activations checkpointing. These're settings found on
+## Nemo repo's Jenkin script.
+##
+## Below settings is similar to 22.09, except that 22.09 funnily didn't OOM with
+## activations_checkpoint_num_layers=0.
+# declare -a MODEL_ARGS=(
+#     training.model.activations_checkpoint_granularity='full'
+#     training.model.activations_checkpoint_method='block'
+#     training.model.activations_checkpoint_num_layers=1
+# )
+
+
+################################################################################
+# 010: Advance users can modify this stanza to customize benchmarking behavior.
+################################################################################
 declare -a BMK_ARGS=(
     # Disable validation, as we're only interested to measure the training time.
     training.trainer.limit_val_batches=0.0
@@ -30,6 +55,16 @@ declare -a BMK_ARGS=(
     training.model.data.data_prefix=[]
 )
 
+
+################################################################################
+# 020: Internal settings.
+################################################################################
+WORKSPACE_CONT=$TARGET_PATH
+CONT_RESULT_DIR=${WORKSPACE_CONT}/results
+CONT_TOKENIZER_DIR=${WORKSPACE_CONT}/data/bpe
+
+# Dev/test feature (off by default) to force each pre-training run outputs to a separate directory.
+: "${UNIQUE_OUTPUT_DIR:=0}"
 if [[ ${UNIQUE_OUTPUT_DIR} -eq 1 ]]; then
     # For debugging: each run has its own output dir.
     TIMESTAMP=$(date +'%Y%m%d-%H%M%Sutc-%N')-$((RANDOM))
@@ -44,11 +79,14 @@ if [[ ${UNIQUE_OUTPUT_DIR} -eq 1 ]]; then
     "
 fi
 
-HYDRA_FULL_ERROR=1 python3 /fsx/nemo-launcher-23.07/launcher_scripts/main.py \
+
+################################################################################
+# 030: Here we go...
+################################################################################
+HYDRA_FULL_ERROR=1 python3 $TARGET_PATH/launcher_scripts/main.py \
     stages=[training] \
     training=${MODEL}/${MODEL_SIZE} \
     training.trainer.num_nodes=$NUM_NODES \
     training.trainer.max_steps=$MAX_STEPS \
     training.trainer.val_check_interval=$MAX_STEPS \
-    "${BMK_ARGS[@]}" \
-    "$@"
+    "${BMK_ARGS[@]}" "${MODEL_ARGS[@]}" "$@"
