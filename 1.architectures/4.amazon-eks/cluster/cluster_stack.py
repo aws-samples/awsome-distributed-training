7d5ea7586e8c0408fc4a988c77352becc247b4c3
Pierre-Yves Aquilanti <pierreya@amazon.com>
2023-09-15 11:55:26 -0500

    Update to use cdk config for parameters

--- 1.architectures/4.amazon-eks/cluster/cluster_stack.py
+++ 1.architectures/4.amazon-eks/cluster/cluster_stack.py
@@ -1,48 +1,59 @@
 from aws_cdk import (
-    # Duration,
     Stack,
-    # aws_sqs as sqs,
+    Fn,
+    aws_eks as eks,
+    aws_ec2 as ec2,
+    aws_iam as iam
 )
 
 from constructs import Construct
 
-import aws_cdk.aws_eks as eks
-import aws_cdk.aws_ec2 as ec2
-import aws_cdk.aws_iam as iam
 import sys
-sys.path.append('../')
-import config
+
 
 class ClusterStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ClusterQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
-
-        # EKS Cluster example: one-liner with default node group
-        #cluster = eks.Cluster(self,"HelloEKS", version=eks.KubernetesVersion.V1_27, default_capacity=2, default_capacity_instance=ec2.InstanceType.of(ec2.InstanceClass.M5,ec2.InstanceSize.SMALL)) 
-
         # Lookup VPC
-        #my_vpc = ec2.Vpc.from_lookup(self,"VPC",vpc_id="vpc-*****************")
-        my_vpc = ec2.Vpc.from_lookup(self,"VPC",vpc_name=config.vpc_name)
+
+        vpc_id = self.node.try_get_context("vpc_id")
+        vpc = ec2.Vpc.from_lookup(self,"VPC",vpc_id=vpc_id)
 
         # Role to access cluster
-        admin_role = iam.Role(self, id=config.eks_master_role_name, role_name=config.eks_master_role_name, assumed_by=iam.AccountRootPrincipal(), description="Role to allow admin access to EKS cluster")
+        eks_master_role_name = self.node.try_get_context("eks_master_role_name")
+        admin_role = iam.Role(self, id=eks_master_role_name,
+                              role_name=eks_master_role_name,
+                              assumed_by=iam.AccountRootPrincipal(),
+                              description="Role to allow admin access to EKS cluster")
+
+        eks_cluster_name = self.node.try_get_context("eks_cluster_name")
+        eks_version_config = self.node.try_get_context("eks_kubernetes_version")
+        eks_version = eks.KubernetesVersion.of(eks_version_config)
 
         # EKS Cluster example: separate cluster and custom nodegroup creation
-        cluster = eks.Cluster(self, id=config.eks_cluster_name, cluster_name=config.eks_cluster_name, version=config.eks_kubernetes_version, default_capacity=0, vpc=my_vpc, masters_role=admin_role, output_cluster_name=True,output_config_command=True, output_masters_role_arn=True )
+        cluster = eks.Cluster(self, id=eks_cluster_name,
+                              cluster_name=eks_cluster_name,
+                              version=eks_version,
+                              default_capacity=0,
+                              vpc=vpc,
+                              masters_role=admin_role,
+                              output_cluster_name=True,
+                              output_config_command=True,
+                              output_masters_role_arn=True)
+
+        eks_sys_ng_instance_type = self.node.try_get_context("eks_sys_ng_instance_type")
+        eks_sys_ng_min_size = self.node.try_get_context("eks_sys_ng_min_size")
+        eks_sys_ng_desired_size = self.node.try_get_context("eks_sys_ng_desired_size")
+        eks_sys_ng_max_size = self.node.try_get_context("eks_sys_ng_max_size")
+        eks_sys_ng_disk_size = self.node.try_get_context("eks_sys_ng_disk_size")
+
+
         cluster.add_nodegroup_capacity("sys-node-group",
-                                       instance_types=[ec2.InstanceType(config.eks_sys_ng_instance_type)],
-                                       min_size=config.eks_sys_ng_min_size,
-                                       desired_size=config.eks_sys_ng_desired_size,
-                                       max_size=config.eks_sys_ng_max_size,
-                                       disk_size=config.eks_sys_ng_disk_size,
-                                       #ami_type=config.eks_sys_ng_ami_type
+                                       instance_types=[ec2.InstanceType(eks_sys_ng_instance_type)],
+                                       min_size=eks_sys_ng_min_size,
+                                       desired_size=eks_sys_ng_desired_size,
+                                       max_size=eks_sys_ng_max_size,
+                                       disk_size=eks_sys_ng_disk_size,
         ) 
--- 1.architectures/4.amazon-eks/tests/unit/test_cluster_stack.py
+++ 1.architectures/4.amazon-eks/tests/unit/test_cluster_stack.py
@@ -5,11 +5,7 @@ from cluster.cluster_stack import ClusterStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in cluster/cluster_stack.py
-def test_sqs_queue_created():
+def test_cluster_created():
     app = core.App()
     stack = ClusterStack(app, "cluster")
     template = assertions.Template.from_stack(stack)
-
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
