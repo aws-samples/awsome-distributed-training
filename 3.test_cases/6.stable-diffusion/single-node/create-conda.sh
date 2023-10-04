712621edf26f67c0aba690415a749b07ff3a9069
Ankur Srivastava <awsankur@amazon.com>
2023-10-04 10:46:32 -0700

    Stable Diffusion
    Authors:
    Ankur Srivastava <awsankur@amazon.com>
    Sean Smith <seaam@amazon.com>

---
+++ 3.test_cases/6.stable-diffusion/single-node/create-conda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+
+conda create -n pt-nightlies python=3.10
+
+conda activate pt-nightlies
+
+# Install PyTorch Nightly distribution with Cuda 12.1
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu121
+
+# Install Diffusers and Transformers
+pip3 install diffusers["torch"] transformers
+
+# Install Weights and Biases
+pip3 install wandb
+
+# We will install Composer from source. First clone the Repo
+git clone https://github.com/mosaicml/composer.git
+
+
+
+
