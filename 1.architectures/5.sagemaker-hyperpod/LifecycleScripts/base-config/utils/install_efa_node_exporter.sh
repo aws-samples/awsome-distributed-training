45115177c4de8888c0c6009ac87a923334719d44
nghtm <nghtm@amazon.com>
2024-02-05 18:32:28 -0500

    Updates to dcgm_exporter, parameterized prometheus version, build docker for efa exporter, update readme. ready for review
    Signed-off-by: nghtm <nghtm@amazon.com>

---
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/install_efa_node_exporter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/aws-samples/awsome-distributed-training.git || { echo "Failed to clone the repository"; exit 1; }
+# Change directory to the desired location
+cd awsome-distributed-training/4.validation_and_observability/3.efa-node-exporter || { echo "Failed to change directory"; exit 1; }
+
+# Build the Docker image explicitly
+sudo docker build -t node_exporter_efa:latest . || { echo "Failed to build Docker image"; exit 1; }
+
+# Run the Docker container with appropriate configurations
+sudo docker run -d \
+  --net="host" \
+  --pid="host" \
+  -v "/:/host:ro,rslave" \
+  node_exporter_efa:latest \
+  --path.rootfs=/host && { echo "Successfully started EFA Node Exporter on node"; exit 0; } || { echo "Failed to run Docker container"; exit 1; }
+
