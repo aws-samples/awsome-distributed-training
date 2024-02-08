5ead5abdc842230062969e7e5f4104aac3a174ba
Verdi March <marcverd@amazon.com>
2024-02-08 12:38:15 +0000

    Megatron-LM Llama2 example
    Make the example work on as low as 2 nodes (16x GPUs each with 80GB memory)

---
+++ 3.test_cases/1.megatron-lm/3.data-preproc-llama2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
+#SBATCH -N 1 # number of nodes we want
+#SBATCH --exclusive # job has exclusive use of the resource, no sharing
+
+###########################
+###### User Variables #####
+###########################
+
+: "${IMAGE:=$(pwd)/megatron-training.sqsh}"
+: "${FSX_MOUNT:=/fsx:/fsx}"
+
+# default variables for Enroot
+: "${DATA_PATH:=/fsx}"
+
+declare -a ARGS=(
+    --container-image $IMAGE
+    --container-mount-home
+    --container-mounts $FSX_MOUNT
+)
+
+# runs in
+srun -l "${ARGS[@]}"  python3 /workspace/Megatron-LM/tools/preprocess_data.py \
+        --input ${DATA_PATH}/llama2/oscar-1GB.jsonl \
+        --output-prefix ${DATA_PATH}/llama2/my-llama2 \
+        --tokenizer-type Llama2Tokenizer \
+        --tokenizer-model ${DATA_PATH}/llama2/tokenizer.model \
+        --append-eod \
+        --workers 64
