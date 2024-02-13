839723a988b64a1292ce13a47b4fd2ad5eb9768d
Maxime Hugues <maxhaws@amazon.com>
2024-02-13 14:10:30 -0600

    Add JAX container build instructions

--- 2.ami_and_containers/containers/jax/run_paxml.sh
+++ 2.ami_and_containers/containers/jax/run_paxml.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 set -ex
 
-# Create checkpoint directry
-mkdir -p "${BASE_DIR}/checkpoints"
 
 # TRAINING_CONFIG example paxml.tasks.lm.params.lm_cloud.LmCloudSpmd2B
 python3.10 -m paxml.main  \
   --job_log_dir="${BASE_DIR}/LOG_DIR" \
   --fdl_config=${TRAINING_CONFIG} \
-  $JAX_FLAGS \
+  ${JAX_FLAGS} \
   --multiprocess_gpu=true \
   --server_addr=${LEAD_NODE}:12345 \
-  --num_hosts=${OMPI_COMM_WORLD_SIZE} \
-  --host_idx=${OMPI_COMM_WORLD_RANK} \
+  --num_hosts=${SLURM_NPROCS} \
+  --host_idx=${SLURM_PROCID} \
   --alsologtostderr
