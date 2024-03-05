4b326968d0eb30d0e8824f16ccf1cdcec0f729c8
Keita Watanabe <mlkeita@amazon.com>
2024-03-06 08:33:14 +0900

    Update 0.create_conda_env.sh

--- 3.test_cases/10.FSDP/0.create_conda_env.sh
+++ 3.test_cases/10.FSDP/0.create_conda_env.sh
@@ -10,7 +10,7 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 
 source ./miniconda3/bin/activate
 
-conda create -y -p ./pt_fsdp python=3.10
+conda create -y -p ./pt_fsdp python=3.10 --strict-channel-priority --override-channels -c https://aws-ml-conda.s3.us-west-2.amazonaws.com -c nvidia -c conda-forge
 
 source activate ./pt_fsdp/
 
