eab94e10290af91bcfe7d5ec623e7a7e6cd780e9
Matthew Nightingale <73195085+nghtm@users.noreply.github.com>
2024-05-13 18:17:58 -0400

    Update install_efa_node_exporter.sh
    Fix file for install_efa_node_exporter.sh

--- 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/install_efa_node_exporter.sh
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/install_efa_node_exporter.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
 
-if nvidia-smi; then
-    echo "NVIDIA GPU found. Proceeding with script..."
+# Define variables
+REPO_DIR="awsome-distributed-training"
+REPO_URL="https://github.com/aws-samples/awsome-distributed-training.git"
 
-    # Get the instance-type from EC2 instance metadata
-    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-    INSTANCE_TYPE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/instance-type)
-
-    # Set DCGM-Exporter-Version, for g5s, use older version (https://github.com/NVIDIA/dcgm-exporter/issues/319)
-    if [[ $INSTANCE_TYPE == *"g5"* ]]; then
-        echo "Instance Type is recognized as $INSTANCE_TYPE setting DCGM_EXPORTER_VERSION to 2.1.4-2.3.1-ubuntu20.04"
-        DCGM_EXPORTER_VERSION=2.1.4-2.3.1-ubuntu20.04
-    else
-        echo "Instance Type is recognized as $INSTANCE_TYPE, setting DCGM_EXPORTER_VERSION to 3.3.5-3.4.0-ubuntu22.04"
-        DCGM_EXPORTER_VERSION=3.3.5-3.4.1-ubuntu22.04
-    fi
-    echo "DCGM_EXPORTER_VERSION = $DCGM_EXPORTER_VERSION"
-
-    # Run the DCGM Exporter Docker container
-    sudo docker run -d --restart always \
-       --gpus all \
-       --net host \
-       --cap-add SYS_ADMIN \
-       nvcr.io/nvidia/k8s/dcgm-exporter:${DCGM_EXPORTER_VERSION} \
-       -f /etc/dcgm-exporter/dcp-metrics-included.csv || { echo "Failed to run DCGM Exporter Docker container"; exit 1; }
-
-    echo "Running DCGM exporter in a Docker container on port 9400..."
+# Check if the repository directory exists
+if [ -d "$REPO_DIR" ]; then
+    echo "Repository already exists, skipping cloning."
 else
-    echo "NVIDIA GPU not found. DCGM Exporter was not installed. If this is a controller node, you can safely ignore this warning. Exiting gracefully..."
-    exit 0
+    # Clone the repository
+    git clone "$REPO_URL" || { echo "Failed to clone the repository"; exit 1; }
 fi
+
+# Change directory to the desired location
+cd "$REPO_DIR/4.validation_and_observability/3.efa-node-exporter" || { echo "Failed to change directory"; exit 1; }
+
+# Build the Docker image explicitly
+sudo docker build -t node_exporter_efa:latest . || { echo "Failed to build Docker image"; exit 1; }
+
+# Run the Docker container with appropriate configurations
+sudo docker run -d --restart always \
+  --net="host" \
+  --pid="host" \
+  -v "/:/host:ro,rslave" \
+  node_exporter_efa:latest \
+  --path.rootfs=/host && { echo "Successfully started EFA Node Exporter on node"; exit 0; } || { echo "Failed to run Docker container"; exit 1; }
