117a44380318d9acfa90b7b6afc4fe6b9e1ecab5
Keita Watanabe <mlkeita@amazon.com>
2024-05-20 21:21:02 +0900

    Update 3.test_cases/19.llama-recipes/scripts/generate_env_vars.sh
    Co-authored-by: Pavel Belevich <belevich@amazon.com>

--- 3.test_cases/19.llama-recipes/scripts/generate_env_vars.sh
+++ 3.test_cases/19.llama-recipes/scripts/generate_env_vars.sh
@@ -11,6 +11,6 @@ export MODEL_PATH=$FSX_PATH/llama3
 export DATA_PATH=\$FSX_PATH/data
 export TEST_CASE_PATH=\${FSX_PATH}/awsome-distributed-training/3.test_cases/19.llama-recipes
 export HF_HOME=\${FSX_PATH}/.cache
-export WANDB_CONFIG_DIR=${FSX_PATH}
+export WANDB_CONFIG_DIR=\${FSX_PATH}
 export WANDB_API_KEY=PUT_YOUR_API_KEY_HERE # You need to place your WANDB_API_KEY here 
 EOF
