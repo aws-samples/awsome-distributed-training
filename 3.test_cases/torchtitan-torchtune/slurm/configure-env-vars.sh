690df840b042710b91158ed8eb3d2f5d105d9c73
Keita Watanabe <mlkeita@amazon.com>
2024-05-25 23:07:56 +0000

    local change

--- 3.test_cases/torchtitan-torchtune/slurm/configure-env-vars.sh
+++ 3.test_cases/torchtitan-torchtune/slurm/configure-env-vars.sh
@@ -6,18 +6,14 @@
 echo "Setting up environment variables"
 
 # Prompt user for WANDB_API_KEY
-echo "Please enter your WANDB_API_KEY"
-read WANDB_API_KEY
+WANDB_API_KEY=$(bash -c 'read -p "Please enter your WANDB_API_KEY: " && echo $REPLY')
 
 echo "export FSX_PATH=/fsx" > .env
 source .env
-echo "export IMAGE=torchtitan-torchtune" >> .env
-source .env
 echo "export APPS_PATH=${FSX_PATH}/apps" >> .env
 source .env
-echo "export ENROOT_IMAGE=$APPS_PATH/${IMAGE}.sqsh" >> .env
-source .env
-echo "export MODEL_PATH=$FSX_PATH/models/torchtitan-torchtune" >> .env
+echo "export MODEL_PATH=$FSX_PATH/models
+torchtitan-torchtune" >> .env
 source .env
 echo "export TEST_CASE_PATH=${FSX_PATH}/awsome-distributed-training/3.test_cases/torchtitan-torchtune/slurm" >> .env
 source .env
@@ -26,8 +22,6 @@ source .env
 echo "export WANDB_CONFIG_DIR=${FSX_PATH}" >> .env
 source .env
 echo "export WANDB_API_KEY=${WANDB_API_KEY}" >> .env
-source .env
-echo "export HF_MODEL=meta-llama/Meta-Llama-3-70B" >> .env
 source .env
 
 echo ".env file created successfully"
