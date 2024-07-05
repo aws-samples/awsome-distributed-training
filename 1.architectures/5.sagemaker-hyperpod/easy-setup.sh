33b633589b7ce38c621e3710b4371df0972acf23
Keita Watanabe <mlkeita@amazon.com>
2024-07-05 19:44:21 +0900

    Add secondary ebs volume setup in easy-setup.sh (#369)
    * Add secondary ebs volume setup in easy-setup.sh
    
    * Update easy-setup.sh

--- 1.architectures/5.sagemaker-hyperpod/easy-setup.sh
+++ 1.architectures/5.sagemaker-hyperpod/easy-setup.sh
@@ -25,6 +25,7 @@ declare -a HELP=(
     "[-s|--stack-id-vpc]"
     "[-i|--instance-type]"
     "[-c|--instance-count]"
+    "[-e|--head-node-ebs-volume-size]"
     "[-d|--dry-run]"
     "CLUSTER_NAME"
 )
@@ -63,6 +64,10 @@ parse_args() {
             INSTANCE_COUNT="$2"
             shift 2
             ;;
+        -e|--head-node-ebs-volume-size)
+            EBS_VOLUME_SIZE="$2"
+            shift 2
+            ;;            
         -d|--dry-run)
             DRY_RUN=1
             shift
@@ -147,6 +152,12 @@ echo "[INFO] INSTANCE = ${INSTANCE}"
 if [ -z ${INSTANCE_COUNT} ]; then
     echo "[WARNING] INSTANCE_COUNTS environment variable is not set, automatically set to 2."
     export INSTANCE_COUNT=2
+fi
+
+# Define EBS_VOLUME_SIZE
+if [ -z ${EBS_VOLUME_SIZE} ]; then
+    echo "[WARNING] EBS_VOLUME_SIZE environment variable is not set, automatically set to 500GB."
+    export EBS_VOLUME_SIZE=500
 fi
 
 # Retrieve VPC ID
@@ -312,6 +323,13 @@ cat > cluster-config.json << EOL
       {
         "InstanceGroupName": "controller-machine",
         "InstanceType": "ml.m5.12xlarge",
+        "InstanceStorageConfigs": [
+          {
+            "EbsVolumeConfig": {
+              "VolumeSizeInGB": ${EBS_VOLUME_SIZE}
+            }
+          }
+        ],        
         "InstanceCount": 1,
         "LifeCycleConfig": {
           "SourceS3Uri": "s3://${BUCKET}/src",
