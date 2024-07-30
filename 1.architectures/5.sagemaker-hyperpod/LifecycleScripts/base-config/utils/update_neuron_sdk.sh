16fdad6e82dc12a5d68096bb36e29f9f725fec38
Keita Watanabe <mlkeita@amazon.com>
2024-07-31 08:58:23 +0900

    Update to the latest instead of 2.19

--- 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/update_neuron_sdk.sh
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/update_neuron_sdk.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 if neuron-top -v &> /dev/null
 then 
-    echo "Update to Neuron SDK Release 2.19.0"
+    echo "Update to the latest Neuron SDK"
     # Configure Linux for Neuron repository updates
     . /etc/os-release
 
@@ -21,13 +21,13 @@ then
     sudo apt-get remove aws-neuronx-oci-hook -y
     sudo apt-get remove aws-neuronx-runtime-lib -y
     sudo apt-get remove aws-neuronx-collectives -y
-    sudo apt-get install aws-neuronx-dkms=2.17.17.0 -y
+    sudo apt-get install aws-neuronx-dkms=2.* -y
     sudo apt-get install aws-neuronx-oci-hook=2.4.4.0 -y
-    sudo apt-get install aws-neuronx-runtime-lib=2.21.41.0* -y
-    sudo apt-get install aws-neuronx-collectives=2.21.46.0* -y
+    sudo apt-get install aws-neuronx-runtime-lib=2.* -y
+    sudo apt-get install aws-neuronx-collectives=2.* -y
 
     # Remove pre-installed package and Install Neuron Tools
     sudo apt-get remove aws-neuron-tools  -y
     sudo apt-get remove aws-neuronx-tools  -y
-    sudo apt-get install aws-neuronx-tools=2.18.3.0 -y
+    sudo apt-get install aws-neuronx-tools=2.* -y
 fi
