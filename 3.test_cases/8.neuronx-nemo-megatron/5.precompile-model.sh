e0e298b285e47cfe674e93fff6a92e533fc3b49f
Keita Watanabe <mlkeita@amazon.com>
2024-07-30 09:37:28 +0900

    Update Llama3 training test case with Trainium on SMHP  (#383)
    * update with Neuron SDK 2.19
    
    * remove nemo-megatron test case in favor of nxd test case

--- 3.test_cases/8.neuronx-nemo-megatron/5.precompile-model.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ${APPS_PATH}/neuronx-nemo-megatron/nemo/examples/nlp/language_modeling
-source ${APPS_PATH}/aws_neuron_venv_pytorch/bin/activate
-sbatch --exclusive --nodes 4 --output ${TEST_CASE_PATH}/slurm-%x-%j.out compile.slurm ./llama_7b.sh
