da8c21cd9c842664819cc3ea495c8cdbe33ce32d
Tomonori Shimomura <59209764+shimomut@users.noreply.github.com>
2024-09-10 15:20:48 -0700

    Use secondary EBS volume for containerd data root if available (#424)

--- 1.architectures/7.sagemaker-hyperpod-eks/LifecycleScripts/base-config/on_create.sh
+++ 1.architectures/7.sagemaker-hyperpod-eks/LifecycleScripts/base-config/on_create.sh
@@ -12,5 +12,11 @@ logger() {
 }
 
 logger "[start] on_create.sh"
+
+if [[ $(mount | grep /opt/sagemaker) ]]; then
+  logger "Found secondary EBS volume. Setting containerd data root to /opt/sagemaker/containerd/data-root"
+  sed -i -e "/^[# ]*root\s*=/c\root = \"/opt/sagemaker/containerd/data-root\"" /etc/eks/containerd/containerd-config.toml
+fi
+
 logger "no more steps to run"
 logger "[stop] on_create.sh"
