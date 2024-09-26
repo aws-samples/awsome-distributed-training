866517d3e63abe3d32dd1cf4138787cb2a6b43dd
nghtm <nghtm@amazon.com>
2024-09-25 20:17:55 -0400

    Updates to Observability scripts include: add idempotency, improve logging, use pre-built ECR image for EFA-Node-Exporter, update dcgm container version to latest from nvidia. tested on g5.48xl and p5.48xl
    Signed-off-by: nghtm <nghtm@amazon.com>

--- 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/install_efa_node_exporter.sh
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/install_efa_node_exporter.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-# Define variables
-REPO_DIR="awsome-distributed-training"
-REPO_URL="https://github.com/aws-samples/awsome-distributed-training.git"
+# Define the container name
+CONTAINER_NAME="efa-node-exporter"
 
-# Check if the repository directory exists
-if [ -d "$REPO_DIR" ]; then
-    echo "Repository already exists, skipping cloning."
+# Check if the container exists and is running
+if docker ps --filter "name=$CONTAINER_NAME" --filter "status=running" | grep -q "$CONTAINER_NAME"; then
+    echo "Container $CONTAINER_NAME is already running."
 else
-    # Clone the repository
-    git clone --depth=1 "$REPO_URL" || { echo "Failed to clone the repository"; exit 1; }
+    echo "Container $CONTAINER_NAME is not running or does not exist..."
+    echo "Checking if $CONTAINER_NAME container exists but is not running. If yes, removing it..."
+    docker rm -f $CONTAINER_NAME && echo "Container $CONTAINER_NAME has been removed."
+    echo "Proceeding with script..."
+    
+    # Run the Docker container with appropriate configurations
+    sudo docker run -d --restart always \
+    --name=$CONTAINER_NAME \
+    --net="host" \
+    --pid="host" \
+    -v "/:/host:ro,rslave" \
+    public.ecr.aws/hpc-cloud/efa-node-exporter:latest \
+    --path.rootfs=/host && { echo "Successfully started EFA Node Exporter on node"; exit 0; } || { echo "Failed to run Docker container"; exit 1; }
 fi
-
-# Change directory to the desired location
-cd "$REPO_DIR/4.validation_and_observability/3.efa-node-exporter" || { echo "Failed to change directory"; exit 1; }
-
-# Build the Docker image explicitly
-sudo docker build -t node_exporter_efa:latest . || { echo "Failed to build Docker image"; exit 1; }
-
-# Run the Docker container with appropriate configurations
-sudo docker run -d --restart always \
-  --net="host" \
-  --pid="host" \
-  -v "/:/host:ro,rslave" \
-  node_exporter_efa:latest \
-  --path.rootfs=/host && { echo "Successfully started EFA Node Exporter on node"; exit 0; } || { echo "Failed to run Docker container"; exit 1; }
