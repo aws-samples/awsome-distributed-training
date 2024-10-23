2a20863861392de582f05fe4e785dc672813dea8
Keita Watanabe <mlkeita@amazon.com>
2024-10-23 16:49:47 +0900

    Update PCluster deployment guide

---
+++ 1.architectures/2.aws-parallelcluster/deployment-guides/post-install-scripts/configure_pyxis_enroot_computenode.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# see https://docs.aws.amazon.com/parallelcluster/latest/ug/tutorials_11_running-containerized-jobs-with-pyxis.html for guidanc
+set -e
+
+echo "Executing $0"
+
+# Configure Enroot
+ENROOT_PERSISTENT_DIR="/var/enroot"
+ENROOT_VOLATILE_DIR="/run/enroot"
+
+sudo mkdir -p $ENROOT_PERSISTENT_DIR
+sudo chmod 1777 $ENROOT_PERSISTENT_DIR
+sudo mkdir -p $ENROOT_VOLATILE_DIR
+sudo chmod 1777 $ENROOT_VOLATILE_DIR
+sudo mv /opt/parallelcluster/examples/enroot/enroot.conf /etc/enroot/enroot.conf
+sudo chmod 0644 /etc/enroot/enroot.conf
+
+# Configure Pyxis
+PYXIS_RUNTIME_DIR="/run/pyxis"
+
+sudo mkdir -p $PYXIS_RUNTIME_DIR
+sudo chmod 1777 $PYXIS_RUNTIME_DIR
+                
