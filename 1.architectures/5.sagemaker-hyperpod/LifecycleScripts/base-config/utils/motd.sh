953b38f70b88e8fb352a12b9f92ae93d46035a10
Matthew Nightingale <73195085+nghtm@users.noreply.github.com>
2024-10-28 20:19:42 -0400

    Update motd.sh
    Add hostname to output of motd.sh

--- 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/motd.sh
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/motd.sh
@@ -6,7 +6,11 @@ sudo cp utils/motd.txt /etc/motd
 token=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 instance_type=$(curl -H "X-aws-ec2-metadata-token: $token" -v http://169.254.169.254/latest/meta-data/instance-type)
 
+# Get hostname
+hostname=$(hostname)
+
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 echo -e "You're on the ${GREEN}$1${ENDCOLOR}" | sudo tee -a /etc/motd
+echo -e "Hostname: ${GREEN}${hostname}${ENDCOLOR}" | sudo tee -a /etc/motd
 echo -e "Instance Type: ${GREEN}ml.${instance_type}${ENDCOLOR}" | sudo tee -a /etc/motd
