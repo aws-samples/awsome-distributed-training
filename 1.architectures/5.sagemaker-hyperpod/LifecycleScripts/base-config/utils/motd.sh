cb3e3f907a6e4c7eb9c794eda1843c49a11cda7b
Sean Smith <seaam@amazon.com>
2024-10-29 11:52:20 -0800

    don't display login in motd if no login node
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/motd.sh
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/motd.sh
@@ -10,5 +10,5 @@ GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 echo -e "You're on the ${GREEN}$1${ENDCOLOR}" | sudo tee -a /etc/motd
 echo -e "Controller Node IP: ${GREEN}$2${ENDCOLOR}" | sudo tee -a /etc/motd
-echo -e "Login Node IP: ${GREEN}$3${ENDCOLOR}" | sudo tee -a /etc/motd
+[ ! -z "$3" ] && echo -e "Login Node IP: ${GREEN}$3${ENDCOLOR}" | sudo tee -a /etc/motd
 echo -e "Instance Type: ${GREEN}ml.${instance_type}${ENDCOLOR}" | sudo tee -a /etc/motd
