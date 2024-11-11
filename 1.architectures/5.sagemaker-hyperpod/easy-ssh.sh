2e5b926d16fcac02cac09867bf57b52d63fda74d
Sean Smith <seaam@amazon.com>
2024-11-11 11:25:50 -0900

    add keypair to ~/.ssh/config file (#485)
    Signed-off-by: Sean Smith <seaam@amazon.com>

--- 1.architectures/5.sagemaker-hyperpod/easy-ssh.sh
+++ 1.architectures/5.sagemaker-hyperpod/easy-ssh.sh
@@ -55,28 +55,62 @@ parse_args() {
     [[ "$cluster_name" == "" ]] && { echo "Must define a cluster name" ; exit -1 ;  }
 }
 
+# Function to check if ml-cluster exists in ~/.ssh/config
+check_ssh_config() {
+    if grep -q "Host ${cluster_name}" ~/.ssh/config; then
+        echo -e "${BLUE}1. Detected ${GREEN}${cluster_name}${BLUE} in  ${GREEN}~/.ssh/config${BLUE}. Skipping adding...${NC}"
+
+        echo -e "\nFYI instead of this script you can do:\n"
+        echo -e "$ ${GREEN}ssh ${cluster_name}${NC}"
+    else
+        echo -e "${BLUE}Would you like to add ${GREEN}${cluster_name}${BLUE} to  ~/.ssh/config (yes/no)?${NC}"
+        read -p "> " ADD_CONFIG
+
+        if [[ $ADD_CONFIG == "yes" ]]; then
+            echo -e "${GREEN}✅ adding ml-cluster to  ~/.ssh/config:${NC}"
+            cat <<EOL >> ~/.ssh/config 
+Host ${cluster_name}
+    User ubuntu
+    ProxyCommand sh -c "aws ssm start-session ${aws_cli_args[@]} --target sagemaker-cluster:${cluster_id}_${node_group}-${instance_id} --document-name AWS-StartSSHSession --parameters 'portNumber=%p'"
+EOL
+            echo -e "\nNext add your ssh public key to ~/.ssh/authorized_keys on the cluster and then you can do:\n"
+            echo -e "$ ${GREEN}ssh ${cluster_name}${NC}"
+        else
+            echo -e "${GREEN}❌ skipping adding ml-cluster to  ~/.ssh/config:"
+        fi      
+    fi
+}
+
 parse_args $@
+
+#===Style Definitions===
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+# Function to print a yellow header
+print_header() {
+    echo -e "\n${BLUE}=================================================${NC}"
+    echo -e "\n${YELLOW}==== $1 ====${NC}\n"
+    echo -e "\n${BLUE}=================================================${NC}"
+
+}
+
+
+print_header "🚀 HyperPod Cluster Easy SSH Script! 🚀"
+
 cluster_id=$(aws sagemaker describe-cluster "${aws_cli_args[@]}" --cluster-name $cluster_name | jq '.ClusterArn' | awk -F/ '{gsub(/"/, "", $NF); print $NF}')
 instance_id=$(aws sagemaker list-cluster-nodes "${aws_cli_args[@]}" --cluster-name $cluster_name --instance-group-name-contains ${node_group} | jq '.ClusterNodeSummaries[0].InstanceId' | tr -d '"')
 
-echo "Cluster id: ${cluster_id}"
-echo "Instance id: ${instance_id}"
-echo "Node Group: ${node_group}"
+# print_header
+echo -e "Cluster id: ${GREEN}${cluster_id}${NC}"
+echo -e "Instance id: ${GREEN}${instance_id}${NC}"
+echo -e "Node Group: ${GREEN}${node_group}${NC}"
 
-echo -e "aws ssm start-session "${aws_cli_args[@]}" --target sagemaker-cluster:${cluster_id}_${node_group}-${instance_id}"
+echo -e "\naws ssm start-session "${aws_cli_args[@]}" --target sagemaker-cluster:${cluster_id}_${node_group}-${instance_id}\n"
 
-echo -e "Add the following to your ~/.ssh/config to easily connect:"
-echo "
-cat <<EOF >> ~/.ssh/config
-Host ${cluster_name}
-  User ubuntu
-  ProxyCommand sh -c \"aws ssm start-session ${aws_cli_args[@]} --target sagemaker-cluster:${cluster_id}_${node_group}-${instance_id} --document-name AWS-StartSSHSession --parameters 'portNumber=%p'\"
-EOF
-
-Add your ssh keypair and then you can do:
-
-$ ssh ${cluster_name}
-"
+check_ssh_config
 
 [[ DRY_RUN -eq 1 ]] && exit 0
 
