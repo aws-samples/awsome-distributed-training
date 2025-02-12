2683e73afe32d3e4248696722c9398e9f7b05324
Nisha <nisha.nadkarni@gmail.com>
2025-02-12 15:40:14 -0800

    Revert "updating task governance"
    This reverts commit 69c5058bc0688d64cc140ef147433c2ef258d60f.

--- 1.architectures/7.sagemaker-hyperpod-eks/task-governance/result/hyperpod-cli-mnist-team-b/hyperpod-cli-mnist-team-b_submission.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-helm install --timeout=15m --wait  --namespace hyperpod-ns-team-b hyperpod-cli-mnist-team-b /Users/nadknish/repos/workshop-validation/awsome-distributed-training/1.architectures/7.sagemaker-hyperpod-eks/task-governance/result/hyperpod-cli-mnist-team-b/k8s_template
