f2d0697f43c7dfd020f154235b721fcbe83b2abe
Keita Watanabe <mlkeita@amazon.com>
2025-02-18 15:45:30 +0900

    avoid redownloading conda binary

--- 3.test_cases/10.FSDP/0.create_conda_env.sh
+++ 3.test_cases/10.FSDP/0.create_conda_env.sh
@@ -4,7 +4,10 @@ set -ex
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+# Check if Miniconda installer exists locally
+if [ ! -f Miniconda3-latest-Linux-x86_64.sh ]; then
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+fi
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh -b -f -p ./miniconda3
 
