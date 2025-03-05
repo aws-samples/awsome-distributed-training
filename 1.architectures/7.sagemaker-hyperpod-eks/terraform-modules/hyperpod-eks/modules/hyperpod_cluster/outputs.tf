e63f5f5754b588ef0971dc4b52c6d94ec27c05f4
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2025-03-05 09:15:24 -0500

    made output and variable corrections

--- 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks/modules/hyperpod_cluster/outputs.tf
+++ 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks/modules/hyperpod_cluster/outputs.tf
@@ -12,3 +12,9 @@ output "instance_group_names" {
   description = "Names of all instance groups created"
   value       = keys(var.instance_groups)
 }
+
+output "hyperpod_cluster_status" {
+  description = "The status of the HyperPod cluster"
+  value       = awscc_sagemaker_cluster.hyperpod_cluster.cluster_status
+
+}
