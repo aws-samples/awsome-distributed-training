efcddaec66a24d5590f6c03b042b77097260b38b
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2025-03-05 09:15:24 -0500

    made output and variable corrections

--- 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks/modules/s3_bucket/outputs.tf
+++ 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks/modules/s3_bucket/outputs.tf
@@ -2,3 +2,8 @@ output "s3_bucket_name" {
   description = "S3 Bucket Name"
   value       = aws_s3_bucket.bucket.id
 }
+
+output "s3_bucket_arn" {
+  description = "S3 Bucket ARN"
+  value       = aws_s3_bucket.bucket.arn
+}
--- 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks/providers.tf
+++ 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks/providers.tf
@@ -8,9 +8,13 @@ provider "awscc" {
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-    token                 = data.aws_eks_cluster_auth.cluster.token
+    host                   = var.create_eks ? module.eks_cluster[0].eks_cluster_endpoint :  data.aws_eks_cluster.existing_eks_cluster[0].endpoint
+    cluster_ca_certificate = base64decode(var.create_eks ? module.eks_cluster[0].eks_cluster_certificate_authority : data.aws_eks_cluster.existing_eks_cluster[0].certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", var.create_eks ? module.eks_cluster[0].eks_cluster_name : var.eks_cluster_name]
+      command     = "aws"
+    }
   }
 }
 
