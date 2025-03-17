2d03710a238ce733875c7b60e6e5837639607661
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2025-03-17 15:05:35 -0400

    remane parent directory hyperpod-eks-tf

--- 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks/modules/sagemaker_iam_role/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sagemaker_iam_role_name" {
-  description = "SageMaker IAM role Name"
-  value       = aws_iam_role.sagemaker_execution_role.name
-}
-
-output "sagemaker_iam_role_arn" {
-  description = "SageMaker IAM role Arn"
-  value       = aws_iam_role.sagemaker_execution_role.arn
-}
