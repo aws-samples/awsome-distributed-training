8ac656b2f3c735cdc51ad37bb4dbdfc1ed15b775
Maxime Hugues <maxhaws@amazon.com>
2025-04-03 14:46:59 -0500

    Change FSDP structure in Kubernetes and Slurm

--- 3.test_cases/pytorch/FSDP/slurm/create_venv.sh
+++ 3.test_cases/pytorch/FSDP/slurm/create_venv.sh
@@ -8,8 +8,7 @@ set -ex
 python3 -m venv env
 source ./env/bin/activate
 
-pip install transformers==4.50.3 datasets fsspec==2023.9.2 python-etcd numpy==1.*
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install -r src/requirements.txt
 
 # Create checkpoint dir
 mkdir checkpoints
