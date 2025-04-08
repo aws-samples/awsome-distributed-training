0011d06841be18d6c4359b171f63ffd06f12ab14
mhuguesaws <71357145+mhuguesaws@users.noreply.github.com>
2025-04-08 09:24:48 -0500

    Change FSDP to install python venv.

--- 3.test_cases/pytorch/FSDP/create_venv.sh
+++ 3.test_cases/pytorch/FSDP/create_venv.sh
@@ -4,6 +4,9 @@ set -ex
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: MIT-0
 
+PYTHON_VERSION=$(python --version | awk '{print $2}' | awk -F'.' '{print $1"."$2}')
+sudo apt install -y python$PYTHON_VERSION-venv
+
 # Create and actiate Python virtual environment
 python3 -m venv env
 source ./env/bin/activate
