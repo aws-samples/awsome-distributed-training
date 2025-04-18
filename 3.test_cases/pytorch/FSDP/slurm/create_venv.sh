fbb2831887b583058f4d91f64705f285172b3723
mhuguesaws <71357145+mhuguesaws@users.noreply.github.com>
2025-04-18 14:34:16 -0500

    Improvements/fsdp restructure (#630)
    * Change FSDP structure in Kubernetes and Slurm
    
    * add req
    
    * Fix req
    
    * Rename training FSDP
    
    * Add container option
    
    * Add model submission file generator
    
    * Change training files for FSDP
    
    * Change FSDP structure and add kubernetes manifest
    
    * Change FSDP main readme
    
    * Fix mathstral model name
    
    * Change FSDP slurm readme
    
    * Update 3.test_cases/pytorch/FSDP/README.md
    
    * rename README
    
    * Update 3.test_cases/pytorch/FSDP/slurm/create_venv.sh
    
    * Add mathstral
    
    * Fix doc
    
    * Complete FSDP slurm doc
    
    * change FSDP Kubernetes doc
    
    ---------
    
    Co-authored-by: Keita Watanabe <mlkeita@amazon.com>

--- 3.test_cases/pytorch/FSDP/slurm/create_venv.sh
+++ 3.test_cases/pytorch/FSDP/slurm/create_venv.sh
@@ -11,8 +11,7 @@ sudo apt install -y python$PYTHON_VERSION-venv
 python3 -m venv env
 source ./env/bin/activate
 
-pip install transformers==4.50.3 datasets fsspec==2023.9.2 python-etcd numpy==1.*
-pip install torch==2.5.1 torchvision==0.20.1 torchaudio==2.5.1 --index-url https://download.pytorch.org/whl/cu124
+pip install -r ../src/requirements.txt
 
 # Create checkpoint dir
 mkdir checkpoints
