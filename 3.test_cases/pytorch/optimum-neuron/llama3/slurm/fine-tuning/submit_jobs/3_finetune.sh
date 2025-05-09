210e9a2c0d9181fc52669a91a0bd70cafed09608
Haotian An <33510317+Captainia@users.noreply.github.com>
2025-05-09 16:06:55 -0400

    Update 3.test_cases/pytorch/optimum-neuron/llama3/slurm/fine-tuning/submit_jobs/3_finetune.sh
    Co-authored-by: Keita Watanabe <keitaw09@gmail.com>

--- 3.test_cases/pytorch/optimum-neuron/llama3/slurm/fine-tuning/submit_jobs/3_finetune.sh
+++ 3.test_cases/pytorch/optimum-neuron/llama3/slurm/fine-tuning/submit_jobs/3_finetune.sh
@@ -7,4 +7,9 @@
 export OMP_NUM_THREADS=1
 export NEURON_EXTRACT_GRAPHS_ONLY=0
 
-srun bash /fsx/ubuntu/awsome-distributed-training/3.test_cases/pytorch/optimum-neuron/llama3/slurm/fine-tuning/llama3-8B/finetune-llama3-8B.sh
+AUTO_RESUME=""
+if [ -d "/opt/sagemaker_cluster" ]; then
+    echo "Detected Hyperpod cluster.. enabling --auto-resume=1"
+    AUTO_RESUME="--auto-resume=1"
+fi
+srun  ${AUTO_RESUME} bash /fsx/ubuntu/awsome-distributed-training/3.test_cases/pytorch/optimum-neuron/llama3/slurm/fine-tuning/llama3-8B/finetune-llama3-8B.sh
