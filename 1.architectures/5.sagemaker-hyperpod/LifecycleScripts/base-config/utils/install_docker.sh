5cb6db2797544b3f70c8a99081c52af07c70e023
Matthew Nightingale <73195085+nghtm@users.noreply.github.com>
2025-05-15 15:58:51 -0400

    Update install_docker.sh
    Force override on the `/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg`
    
    This will over-ride the prompt to overwrite if the file already exists
    ```
    File '/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg' exists. Overwrite? (y/N)
    ```

--- 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/install_docker.sh
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/utils/install_docker.sh
@@ -33,7 +33,7 @@ systemctl enable docker.service
 systemctl start docker.service
 
 # install nvidia docker toolkit
-curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --yes --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
   && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
