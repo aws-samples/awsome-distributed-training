b26ed226d0ff2ecc2d6edce5b0ae41c004a2f15e
Tomonori Shimomura <shimomut@amazon.com>
2025-08-22 18:22:51 +0000

    Create OTEL config from template

--- 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/observability/install_observability.py
+++ 1.architectures/5.sagemaker-hyperpod/LifecycleScripts/base-config/observability/install_observability.py
@@ -2,14 +2,37 @@ import os
 import shutil
 import argparse
 import subprocess
+import socket
 
 
-def install_observability(node_type, advanced=False):
+def create_file_from_template(template_path, output_path, replacements):
+    with open(template_path, 'r') as template_file:
+        
+        template = template_file.read()
+
+    # use Python's string substitution
+    content = template.format(**replacements)
+
+    with open(output_path, 'w') as output_file:
+        output_file.write(content)
+
+
+def install_observability(node_type, region, prometheus_remote_write_url, advanced=False):
+
+    hostname = socket.gethostname()
 
     if node_type=="controller":
 
         os.makedirs("/etc/otel", exist_ok=True)
-        shutil.copy("./otel_config/config-head.yaml", "/etc/otel/config.yaml")
+        create_file_from_template(
+            "./otel_config/config-head-template.yaml",
+            "/etc/otel/config.yaml",
+            {
+                "REGION": region,
+                "AMPREMOTEWRITEURL": prometheus_remote_write_url,
+                "HOSTNAME": hostname
+            }
+        )
 
         subprocess.run( ["bash", "./install_node_exporter.sh"] )
         subprocess.run( ["bash", "./install_slurm_exporter.sh"] )
@@ -17,14 +40,22 @@ def install_observability(node_type, advanced=False):
 
     elif node_type=="compute":
 
-        os.makedirs("/etc/otel", exist_ok=True)
-        shutil.copy("./otel_config/config-compute.yaml", "/etc/otel/config.yaml")
-
         os.makedirs("/etc/dcgm-exporter", exist_ok=True)
         if advanced:
             shutil.copy("./dcgm_metrics_config/dcgm-metrics-advanced.csv", "/etc/dcgm-exporter/dcgm-metrics.csv")
         else:
             shutil.copy("./dcgm_metrics_config/dcgm-metrics-basic.csv", "/etc/dcgm-exporter/dcgm-metrics.csv")
+
+        os.makedirs("/etc/otel", exist_ok=True)
+        create_file_from_template(
+            "./otel_config/config-compute-template.yaml",
+            "/etc/otel/config.yaml",
+            {
+                "REGION": region,
+                "AMPREMOTEWRITEURL": prometheus_remote_write_url,
+                "HOSTNAME": hostname
+            }
+        )
 
         subprocess.run( ["bash", "./install_node_exporter.sh"] )
         subprocess.run( ["bash", "./install_dcgm_exporter.sh"] )
@@ -39,6 +70,8 @@ if __name__ == "__main__":
 
     argparser = argparse.ArgumentParser(description="Script to install HyperPod observability")
     argparser.add_argument('--node-type', action="store", required=True, help='Node type (controller, login, compute)')
+    argparser.add_argument('--region', action="store", required=True, help='AWS Region (e.g. us-west-2)')
+    argparser.add_argument('--prometheus-remote-write-url', action="store", required=True, help='Prometheus remote write URL (e.g. https://aps-workspaces.us-west-2.amazonaws.com/workspaces/ws-e37c0ee4-7f7f-4f65-b72b-5455852d0c23/api/v1/remote_write)')
     argparser.add_argument('--advanced', action="store_true", default=False, help='Advanced setup (default: False)')
     args = argparser.parse_args()
 
@@ -46,7 +79,7 @@ if __name__ == "__main__":
 
     print("Starting observability installation")
 
-    install_observability(args.node_type, args.advanced)
+    install_observability(args.node_type, args.region, args.prometheus_remote_write_url, args.advanced)
 
     print("---")
     print("Finished observability installation")
