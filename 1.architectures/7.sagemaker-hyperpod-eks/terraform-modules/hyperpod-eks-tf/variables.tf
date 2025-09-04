6dc9d84ba0452768137b44c27096c25ff17d28fa
Nathaniel Clay Arnold <16687465+bluecrayon52@users.noreply.github.com>
2025-09-04 11:20:09 -0500

    CFN/TF HyperPod EKS Parameter Updates (#841)
    * updates to hpeks cfn, tf, and create_config.sh
    
    * cfn-lint fixes
    
    * updates to hpeks cfn, tf, and create_config.sh
    
    * cfn-lint fixes

--- 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks-tf/variables.tf
+++ 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks-tf/variables.tf
@@ -213,20 +213,53 @@ variable "hyperpod_cluster_name" {
 }
 
 variable "node_recovery" {
-  description = "Node recovery mode"
+  description = "Specifies whether to enable or disable the automatic node recovery feature"
   type        = string
   default     = "Automatic"
+  validation {
+    condition     = contains(["Automatic", "None"], var.node_recovery)
+    error_message = "Node recovery must be either 'Automatic' or 'None'"
+  }
+}
+
+variable "node_provisioning_mode" { 
+  description = "Determines the scaling strategy for the SageMaker HyperPod cluster. When set to 'Continuous', enables continuous scaling which dynamically manages node provisioning. Set to null to disable continuous provisioning and use standard scaling approach."
+  type        = string
+  default     = "Continuous"
+  validation {
+    condition     = var.node_provisioning_mode == null || var.node_provisioning_mode == "Continuous"
+    error_message = "Node provisioning mode must be either 'Continuous' or null"
+  }
 }
 
 variable "instance_groups" {
   description = "Map of instance group configurations"
   type = map(object({
-    instance_type       = string
-    instance_count      = number
-    ebs_volume_size    = number
-    threads_per_core   = number
-    enable_stress_check = bool
+    instance_type             = string
+    instance_count            = number
+    ebs_volume_size_in_gb     = number
+    threads_per_core          = number
+    enable_stress_check       = bool
     enable_connectivity_check = bool
-    lifecycle_script    = string
+    lifecycle_script          = string
+    image_id                  = optional(string)
+    training_plan_arn         = optional(string)
   }))
+  default = {}
+}
+
+variable "restricted_instance_groups" {
+  description = "Map of restricted instance group configurations"
+  type = map(object({
+    instance_type                    = string
+    instance_count                   = number
+    ebs_volume_size_in_gb            = number
+    threads_per_core                 = number
+    enable_stress_check              = bool
+    enable_connectivity_check        = bool
+    fsxl_per_unit_storage_throughput = number
+    fsxl_size_in_gi_b                = number
+    training_plan_arn                = optional(string)
+  }))
+  default = {}
 }
