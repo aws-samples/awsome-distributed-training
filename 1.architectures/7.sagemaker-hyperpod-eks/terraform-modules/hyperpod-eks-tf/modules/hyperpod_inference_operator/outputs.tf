a8021ed3e75e58fc6dcfcf763d57b57d45d19cf4
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2025-12-16 22:27:20 -0600

    added hpto, hpio, logging s3 bucket, and cert-manager eks addon

---
+++ 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks-tf/modules/hyperpod_inference_operator/outputs.tf
@@ -0,0 +1,29 @@
+output "helm_release_name" {
+  description = "Name of the inference operator Helm release"
+  value       = helm_release.inference_operator.name
+}
+
+output "helm_release_status" {
+  description = "Status of the inference operator Helm release"
+  value       = helm_release.inference_operator.status
+}
+
+output "inference_operator_role_arn" {
+  description = "ARN of the inference operator IAM role"
+  value       = aws_iam_role.inference_operator.arn
+}
+
+output "s3_csi_role_arn" {
+  description = "ARN of the S3 CSI driver IAM role"
+  value       = aws_iam_role.s3_csi_sa.arn
+}
+
+output "keda_role_arn" {
+  description = "ARN of the KEDA IAM role"
+  value       = aws_iam_role.keda.arn
+}
+
+output "tls_certificates_bucket_name" {
+  description = "Name of the TLS certificates S3 bucket"
+  value       = aws_s3_bucket.tls_certificates.id
+}
---
+++ 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks-tf/modules/hyperpod_training_operator/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_name_prefix" {
+  description = "Prefix to be used for all resources"
+  type        = string
+}
+
+variable "eks_cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
