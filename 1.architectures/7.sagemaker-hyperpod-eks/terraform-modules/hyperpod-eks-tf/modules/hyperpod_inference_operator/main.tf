51a0e82a013c50dcdcd78cbd16514a76a58956f1
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2025-12-22 06:15:23 -0600

    update hpio policies, add oidc provider, code tidy

--- 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks-tf/modules/hyperpod_inference_operator/main.tf
+++ 1.architectures/7.sagemaker-hyperpod-eks/terraform-modules/hyperpod-eks-tf/modules/hyperpod_inference_operator/main.tf
@@ -9,3 +9,17 @@ locals {
   oidc_provider_url                  = data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer
   oidc_provider_url_without_protocol = replace(local.oidc_provider_url, "https://", "")
 }
+
+data "tls_certificate" "eks_oidc_thumbprint" {
+  url = local.oidc_provider_url
+}
+
+resource "aws_iam_openid_connect_provider" "eks_oidc_provider" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks_oidc_thumbprint.certificates[0].sha1_fingerprint]
+  url             = local.oidc_provider_url
+
+  tags = {
+    Name = "${var.resource_name_prefix}-eks-oidc-provider"
+  }
+}
